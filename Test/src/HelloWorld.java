c085caf117c80b669f08990670d4b689202c1cc0
GrantDuan <duanxiugang@sina.com>
2013-03-28 22:10:42 +0800

    a

--- Test/src/HelloWorld.java
+++ Test/src/HelloWorld.java
@@ -7,7 +7,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         System.out.println("Hellow World!");
-        System.out.println("You are funny!");
+        System.out.println("You are funny!");        
+        StdOut.println("use external lib");
 
     }
 
