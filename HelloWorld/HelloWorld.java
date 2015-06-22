c91109ac11a5205af2b8f4ef57cdc6a7f58eeba5
GrantDuan <duanxiugang@sina.com>
2015-06-22 22:01:29 +0800

    commit

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
     public static void main(String[] args)
     {
         System.out.println("Hello world");
-        //System.out.println("Hello world again");
+        System.out.println("Hello world again");
         
         //add in office
         //ADD AT HOME
