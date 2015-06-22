0be372144ddf40c81f67cd7565970f601be7a10e
GrantDuan <duanxiugang@sina.com>
2015-06-22 22:23:11 +0800

    newBranch

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -5,6 +5,9 @@ public class HelloWorld {
         System.out.println("Hello world");
         System.out.println("Hello world again");
         
+        //add in new branch
+        System.out.println("Hello world from new branch");
+        
         //add in office
         //ADD AT HOME
         //AT HOME
